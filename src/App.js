57d64697ff25e3b3ecc6dd5a194ae067857b00ed
Brianna Loveless <briannabel97@gmail.com>
2022-05-05 11:19:33 -0500

    read and create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { db } from './firebase-config';
+import { collection, getDocs, addDoc } from 'firebase/firestore';
 
 function App() {
-  return (
-    <div>
-     
-    </div>
-  );
+    const [newName, setNewName] = useState('');
+    const [newAge, setNewAge] = useState(0);
+    const [users, setUsers] = useState([]);
+    const usersCollectionRef = collection(db, 'users');
+
+    const createUser = async () => {
+      await addDoc(usersCollectionRef, {name: newName, age: newAge })
+    };
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const data = await getDocs(usersCollectionRef);
+            setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        };
+
+        getUsers();
+    }, []);
+
+    return (
+        <div>
+            <input
+                placeholder="Name..."
+                onChange={(event) => {
+                    setNewName(event.target.value);
+                }}
+            />
+            <input
+                type="number"
+                placeholder="Age..."
+                onChange={(event) => {
+                    setNewAge(event.target.value);
+                }}
+            />
+
+            <button onClick={createUser}>Create User</button>
+
+            {users.map((user) => {
+                return (
+                    <div>
+                        <h1>Name: {user.name}</h1>
+                        <h1>Age: {user.age}</h1>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default App;
